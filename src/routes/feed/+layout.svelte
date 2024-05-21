5e857ab847120a6444b55d341b2de61b35e8440e
Sean Griffin <sgriffin6697@gmail.com>
2024-05-20 20:35:19 -0700

    new sveltekit 4

---
+++ src/routes/feed/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import '../app.css'
+  import { QueryClientProvider } from '@tanstack/svelte-query'
+  import { SvelteQueryDevtools } from '@tanstack/svelte-query-devtools'
+  import type { PageData } from './$types'
+
+  export let data: PageData
+</script>
+
+<QueryClientProvider client={data.queryClient}>
+  <main>
+    <slot />
+  </main>
+  <SvelteQueryDevtools />
+</QueryClientProvider>
