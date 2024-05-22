5309a0e88ec00add0d63a42f091957e3d065ab01
Sean Griffin <sgriffin6697@gmail.com>
2024-05-22 11:08:19 -0700

    shadcn

--- src/lib/FeedItem.svelte
+++ src/lib/FeedItem.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
-	import type { PostView } from "@atproto/api/dist/client/types/app/bsky/feed/defs";
+  	import type { PostView } from "@atproto/api/dist/client/types/app/bsky/feed/defs";
+  import * as Card from "$lib/components/ui/card";
+  import * as Avatar from "$lib/components/ui/avatar";
+
 
   export let post: PostView;
 </script>
 
-<p>{post.author.displayName} ({post.author.handle})</p>
-<p>{post.record.text}</p>
+<Card.Root>
+  <Card.Header>
+    <Avatar.Root>
+      <Avatar.Image src={post.author.avatar} alt={post.author.handle} />
+      <Avatar.Fallback>CN</Avatar.Fallback>
+    </Avatar.Root>
+    <Card.Title>{post.author.displayName} ({post.author.handle})</Card.Title>
+    <Card.Description>Card Description</Card.Description>
+  </Card.Header>
+  <Card.Content>
+    {post.record.text}
+  </Card.Content>
+</Card.Root>
